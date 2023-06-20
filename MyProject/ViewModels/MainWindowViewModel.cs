e39235f60b909309a810b1d363bfe7f1739811d5
praim <praimgg@gmail.com>
2023-06-20 13:57:32 +0300

    Тестирование работы модели представления

---
+++ MyProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyProject.ViewModels.Base;
+
+namespace MyProject.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Header MainWindow
+        private string title = "Create Test";
+
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+        }
+        #endregion
+    }
+}
