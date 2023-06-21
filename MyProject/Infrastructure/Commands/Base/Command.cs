88fbd448c32ec6b5992375a61f534a0adc144030
praim <praimgg@gmail.com>
2023-06-21 11:48:07 +0300

    Базовый класс команды

---
+++ MyProject/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MyProject.Infrastructure.Commands.Base
+{
+    internal class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested += value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
