5c73813eefae1d59b9ce1f4a31d5f211ad898ec6
praim <praimgg@gmail.com>
2023-06-27 12:21:33 +0300

    Добавление CreateTestViewModel

---
+++ MyProject/ViewModels/CreateTestWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MyProject.ViewModels.Base;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace MyProject.ViewModels
+{
+    internal class CreateTestWindowViewModel : ViewModel
+    {
+        public ObservableCollection<object> inerphaseItem = new ObservableCollection<object>()
+        {
+            new TextBlock(),
+            new TextBlock()
+        };
+
+        public CreateTestWindowViewModel()
+        {
+
+        }
+    }
+}
